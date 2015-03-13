2682b0ceb0bdf2f14062688a6f2e0b57a84e36ef
ryanpartridge <ryan.partridge@gmail.com>
2015-03-13 12:01:03 -0600

    finish processing one file

--- tracknum.py
+++ tracknum.py
@@ -17,7 +17,12 @@ It defines classes_and_methods
 @deffield    updated: Updated
 '''
 
-import sys, os.path, string, re, subprocess
+import sys
+import os.path
+import string
+import re
+import subprocess
+import shutil
 from argparse import ArgumentParser
 from argparse import RawDescriptionHelpFormatter
 
@@ -39,95 +44,86 @@ def parseArgs():
   # Setup argument parser
   parser = ArgumentParser(description = program_license, formatter_class = RawDescriptionHelpFormatter)
   parser.add_argument('m4aFile', help = "M4A file to rename")
+  parser.add_argument("dstDir", help = "Destination directory")
 
   return parser.parse_args()
 
 def validateArgs():
+  # is the file specified?
   if not arguments.m4aFile:
-    print >> sys.stderr, 'incorrect parameters -h for help'
+    print >> sys.stderr, "ERROR: must specify an M4A file"
     sys.exit()
 
+  # does the file exist?
   if not os.path.isfile(arguments.m4aFile):
     print >> sys.stderr, "ERROR: '" + arguments.m4aFile + "' does not exist"
     sys.exit()
 
+  # does the file end in .m4a?
   if arguments.m4aFile[-4:] != ".m4a":
     print >> sys.stderr, "ERROR: '" + arguments.m4aFile + "' is not an m4a file"
     sys.exit()
 
+  # is the directory specified?
+  if not arguments.dstDir:
+    print >> sys.stderr, "ERROR: must specify a destination directory"
+    sys.exit()
+
+  # create the directory if it doesn't exist
+  if not os.path.exists(arguments.dstDir):
+    os.makedirs(arguments.dstDir)
+
+  # does the location exist but it's not a directory?
+  elif not os.path.isdir(arguments.dstDir):
+    print >> sys.stderr, "ERROR: '" + arguments.dstDir + "' exists but is not a directory"
+    sys.exit()
+
 def readFileTags():
+  title = ""
+  track = 0
+  trackTotal = 0
   try:
     output = subprocess.check_output(['/usr/bin/mp4info', arguments.m4aFile])
   except subprocess.CalledProcessError as e:
     print >> sys.stderr, "ERROR: couldn't read mp4 tags"
     sys.exit()
 
+  titlePattern = re.compile(r"^\s+Name:\s+(.+)$")
+  trackPattern = re.compile(r"^\s+Track:\s+(\d+)\s+of\s+(\d+).*$")
+
   lines = string.split(output, "\n")
   for line in lines:
-    print "line: " + line 
-  return ("", "")
+    matches = titlePattern.match(line)
+    if (matches != None):
+      title = matches.group(1)
+      print "Track title: " + title
+      continue
 
-def parseTrackFile():
-  tf = open(arguments.trackFile)
-  currentAlbum = '';
-  titlePattern = re.compile(r"^\d\d\s+-\s+(.*)$")
-  for line in tf.readlines():
-    line = string.strip(line)
-    if line == "" and currentAlbum:
-      currentAlbum = ''
-    else:
-      if currentAlbum:
-        matches = titlePattern.match(line)
-        if (matches != None):
-          title = matches.group(1)
-          albums[currentAlbum].append(title)
-      else:
-        currentAlbum = line
-        albums[currentAlbum] = []
+    matches = trackPattern.match(line)
+    if (matches != None):
+      track = int(matches.group(1))
+      trackTotal = int(matches.group(2))
+      print "Track number: " + str(track) + " of " + str(trackTotal)
+      continue
 
-  tf.close()
+  return (title, track, trackTotal)
 
-def writeAlbums():
-  albumNames = albums.keys()
-  albumNames.sort()
-  for album in albumNames:
-    if arguments.albumDir[-1] == os.sep:
-      albumPath = arguments.albumDir + album
-    else:
-      albumPath = arguments.albumDir + os.sep + album
-    if not os.path.isdir(albumPath):
-      print "WARNING: '" + albumPath + "' does not exist or is not a directory -- skipping"
-    else:
-      print "Processing '" + album + "'..."
-      index = 0;
-      tracks = albums[album]
-      trackFiles = os.listdir(albumPath)
-      trackFiles.sort();
-      for trackFile in trackFiles:
-#        print "  " + trackFile + " --> " + tracks[index]
-        command = '/usr/bin/eyeD3 -t "' + tracks[index] + '" "' + albumPath + os.sep + trackFile + '"'
-#        print "  " + command
-        os.system(command)
-        index += 1
-      command = '/usr/bin/eyeD3 --rename="%n - %t" "' + albumPath + '"'
-#      print "  " + command
-      os.system(command)
-      print
-
-def printAlbums():
-  albumNames = albums.keys()
-  albumNames.sort()
-  for album in albumNames:
-    print album
-    for track in albums[album]:
-      print '  ' + track
-    print
+def copyTrackFile():
+  (trackName, trackNum, trackTotal) = readFileTags()
+  trackFileName = ""
+  if (trackTotal > 99 and trackNum < 100):
+      trackFileName = "0"
+  trackFileName = trackFileName + str(trackNum) + " " + trackName + ".m4a"
+  print "Old track name: " + os.path.basename(arguments.m4aFile)
+  print "New track name: " + trackFileName
+  outPath = os.path.join(arguments.dstDir, trackFileName)
+  print "New destination: " + outPath
+  shutil.copyfile(arguments.m4aFile, outPath)
+  st = os.stat(arguments.m4aFile)
+  if hasattr(os, 'utime'):
+    os.utime(outPath, (st.st_atime, st.st_mtime))
 
 if __name__ == "__main__":
-  albums = {};
   arguments = parseArgs()
   validateArgs()
-  (trackName, trackNum) = readFileTags()
-  print "name: " + trackName + " (" + trackNum + ")"
-  #parseTrackFile()
-#writeAlbums()
+  copyTrackFile()
